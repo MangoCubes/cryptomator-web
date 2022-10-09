e546138261fa0d930b03d7e60fe9d65751b8a7d2
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-10-10 01:41:09 +0900

    Added iconbutton to access help

--- src/lib/web/Login.tsx
+++ src/lib/web/Login.tsx
@@ -1,4 +1,5 @@
-import { Button, Card, CardContent, Stack, TextField, Typography } from "@mui/material";
+import { Help } from "@mui/icons-material";
+import { Button, Card, CardContent, FormControl, IconButton, Input, InputAdornment, InputLabel, Stack, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useState } from "react";
 import { WebDAV } from "../cryptomator/storage-adapters/WebDAV";
@@ -16,11 +17,24 @@ export function Login(props: {setClient: (client: WebDAV) => void}){
 
 	return (
 		<Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-    		<Card sx={{width: '40%', height: '60%'}}>
+			<Card sx={{width: '40%', height: '60%'}}>
 				<CardContent sx={{width: '60%', margin: '0 auto'}}>
 					<Stack component='form' onSubmit={login} spacing={2}>
 						<Typography variant='h5'>Enter WebDAV credentials</Typography>
-						<TextField required disabled={querying} variant='standard' label='WebDAV URL' value={url} onChange={(e) => setUrl(e.currentTarget.value)}/>
+						<FormControl variant='standard'>
+							<InputLabel required>WebDAV URL</InputLabel>
+							<Input
+								required
+								disabled={querying}
+								value={url}
+								onChange={(e) => setUrl(e.currentTarget.value)}
+								endAdornment={
+									<InputAdornment position='end'>
+										<IconButton><Help/></IconButton>
+									</InputAdornment>
+								}
+							/>
+						</FormControl>
 						<TextField required disabled={querying} variant='standard' label='Username' autoFocus value={username} onChange={(e) => setUsername(e.currentTarget.value)}/>
 						<TextField required disabled={querying} variant='standard' label='Password' type='password' value={password} onChange={(e) => setPassword(e.currentTarget.value)}/>
 						<Button disabled={querying} type='submit' fullWidth>Connect</Button>
