e654f1e94a99952f845c5dc0570f39a78317c2db
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-10-10 01:32:26 +0900

    Added login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { ThemeProvider } from '@emotion/react';
+import { createTheme, CssBaseline } from '@mui/material';
 import React from 'react';
+import { MainScreen } from './lib/web/MainScreen';
 
 export function App() {
-  	return (
-	<>
-	</>
-  	);
+	const theme = createTheme({
+		palette: {
+			mode: 'dark',
+		}
+	});
+
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<MainScreen/>
+		</ThemeProvider>
+	);
 }
--- src/lib/web/Login.tsx
+++ src/lib/web/Login.tsx
@@ -1,8 +1,32 @@
+import { Button, Card, CardContent, Stack, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
 import { WebDAV } from "../cryptomator/storage-adapters/WebDAV";
 
 export function Login(props: {setClient: (client: WebDAV) => void}){
+
+	const [url, setUrl] = useState('');
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [querying, setQuerying] = useState(false);
+
+	const login = () => {
+
+	}
+
 	return (
-		<>
-		</>
+		<Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+    		<Card sx={{width: '40%', height: '60%'}}>
+				<CardContent sx={{width: '60%', margin: '0 auto'}}>
+					<Stack component='form' onSubmit={login} spacing={2}>
+						<Typography variant='h5'>Enter WebDAV credentials</Typography>
+						<TextField required disabled={querying} variant='standard' label='WebDAV URL' value={url} onChange={(e) => setUrl(e.currentTarget.value)}/>
+						<TextField required disabled={querying} variant='standard' label='Username' autoFocus value={username} onChange={(e) => setUsername(e.currentTarget.value)}/>
+						<TextField required disabled={querying} variant='standard' label='Password' type='password' value={password} onChange={(e) => setPassword(e.currentTarget.value)}/>
+						<Button disabled={querying} type='submit' fullWidth>Connect</Button>
+					</Stack>
+				</CardContent>
+			</Card>
+		</Box>
 	)
 }
