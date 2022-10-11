02455d4e81f1f1a1087b63b9f314a4929371b488
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-10-11 21:04:24 +0900

    Added error messages

--- src/web/Login.tsx
+++ src/web/Login.tsx
@@ -1,9 +1,15 @@
 import { Help } from "@mui/icons-material";
-import { Button, Card, CardContent, FormControl, IconButton, Input, InputAdornment, InputLabel, Stack, TextField, Typography } from "@mui/material";
+import { Button, Card, CardContent, FormControl, FormHelperText, IconButton, Input, InputAdornment, InputLabel, Stack, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { FormEvent, useState } from "react";
 import { WebDAV } from "../lib/cryptomator/storage-adapters/WebDAV";
 import { UrlHelperDialog } from "./helperDialog/UrlHelperDialog";
+import { AxiosError } from "axios";
+
+enum LoginErr {
+	Incorrect,
+	Unknown
+}
 
 export function Login(props: {setClient: (client: WebDAV) => void}){
 
@@ -12,17 +18,29 @@ export function Login(props: {setClient: (client: WebDAV) => void}){
 	const [password, setPassword] = useState('');
 	const [querying, setQuerying] = useState(false);
 	const [open, setOpen] = useState(false);
+	const [error, setError] = useState<LoginErr | null>(null);
 
 	const login = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
+		setQuerying(true);
+		setError(null);
 		const client = new WebDAV(url, username, password);
 		try {
 			await client.list('/'); // Test query
 			props.setClient(client);
 		} catch (e) {
-
+			let err = LoginErr.Unknown;
+			if(e instanceof Error){
+				if((e as Error & {status: number}).status === 401) err = LoginErr.Incorrect;
+			}
+			setError(err);
+			setQuerying(false);
 		}
-		
+	}
+
+	const getHelperText = () => {
+		if(error === LoginErr.Unknown) return 'Cannot connect to server. Check the entered URL, or check CORS policy.';
+		else return ' ';
 	}
 
 	return (
@@ -43,10 +61,12 @@ export function Login(props: {setClient: (client: WebDAV) => void}){
 										<IconButton onClick={() => setOpen(true)}><Help/></IconButton>
 									</InputAdornment>
 								}
+								error={error === LoginErr.Unknown}
 							/>
+							<FormHelperText>{getHelperText()}</FormHelperText>
 						</FormControl>
-						<TextField required disabled={querying} variant='standard' label='Username' autoFocus value={username} onChange={(e) => setUsername(e.currentTarget.value)}/>
-						<TextField required disabled={querying} variant='standard' label='Password' type='password' value={password} onChange={(e) => setPassword(e.currentTarget.value)}/>
+						<TextField required disabled={querying} error={error === LoginErr.Incorrect} variant='standard' label='Username' autoFocus value={username} onChange={(e) => setUsername(e.currentTarget.value)}/>
+						<TextField required disabled={querying} error={error === LoginErr.Incorrect} helperText={error === LoginErr.Incorrect ? 'Incorrect username or password.' : ' '} variant='standard' label='Password' type='password' value={password} onChange={(e) => setPassword(e.currentTarget.value)}/>
 						<Button disabled={querying} type='submit' fullWidth>Connect</Button>
 					</Stack>
 				</CardContent>
