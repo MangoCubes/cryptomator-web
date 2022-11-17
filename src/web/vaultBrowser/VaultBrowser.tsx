6f8702fe83a398992c541eca4df9b83aea542f73
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-11-17 23:38:20 +0900

    Added title, load items function

--- src/web/vaultBrowser/VaultBrowser.tsx
+++ src/web/vaultBrowser/VaultBrowser.tsx
@@ -1,6 +1,7 @@
 import { Refresh } from "@mui/icons-material";
 import { Box, AppBar, Toolbar, Typography, Tooltip, IconButton } from "@mui/material";
-import { DirID, Vault } from "cryptomator-ts";
+import { GridSelectionModel } from "@mui/x-data-grid";
+import { DirID, EncryptedItem, Vault } from "cryptomator-ts";
 import { useState } from "react";
 import { WebDAV } from "../../lib/cryptomator/WebDAV";
 
@@ -11,13 +12,33 @@ type VaultDir = {
 
 export function VaultBrowser(props: {vault: Vault, client: WebDAV}){
 
-	const [dir, setDir] = useState<VaultDir[]>([]);
+	// dir length should never be 0 since first element is the root folder
+	const [dir, setDir] = useState<VaultDir[]>([{
+		name: 'Root',
+		id: '' as DirID
+	}]);
+	const [items, setItems] = useState<EncryptedItem[]>([]);
+	const [querying, setQuerying] = useState(false);
+	const [sel, setSel] = useState<GridSelectionModel>([]);
+
+	const loadItems = async (dirId: DirID) => {
+		const temp = [...items];
+		try {
+			setQuerying(true);
+			setItems([]);
+			setItems(await props.vault.listItems(dirId));
+		} catch(e) {
+			setItems(temp);
+		} finally {
+			setQuerying(false);
+		}
+	}
 
 	return (
 		<Box sx={{display: 'flex', flexDirection: 'column', height: '100%', flex: 1}}>
 			<AppBar position='static'>
 				<Toolbar>
-					<Typography variant='h5'></Typography>
+					<Typography variant='h5'>{`${[props.vault.name]}: ${dir[dir.length - 1]}`}</Typography>
 					<Box sx={{flex: 1}}/>
 					<Tooltip title='Refresh'>
 						<span>
