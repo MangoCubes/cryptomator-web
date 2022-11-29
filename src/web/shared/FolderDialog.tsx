69266f7da3029b40d378cedc34d2512369469842
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-11-29 22:31:53 +0900

    Created a dialog that asks for folder name

---
+++ src/web/shared/FolderDialog.tsx
@@ -0,0 +1,43 @@
+import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button } from "@mui/material";
+import { useState, useEffect } from "react";
+
+export function FolderDialog(props: {open: boolean, close: () => void, create: (name: string) => Promise<void>}){
+
+	const [name, setName] = useState('');
+	const [error, setError] = useState(false);
+	const [querying, setQuerying] = useState(false);
+
+	useEffect(() => {
+		return () => setName('');
+	}, []);
+
+	const onClose = () => {
+		if(!querying) props.close();
+	}
+
+	const create = async () => {
+		try {
+			setQuerying(true);
+			await props.create(name);
+			props.close();
+		} catch (e) {
+			setError(true);
+		} finally {
+			setQuerying(false);
+		}
+	}
+
+	return (
+		<Dialog open={props.open} onClose={onClose}>
+			<DialogTitle>Create new folder</DialogTitle>
+			<DialogContent>
+				<TextField
+					disabled={querying} required variant='standard' label='Name' type='name' value={name} error={error} helperText={error ? 'Cannot create folder.' : ' '}/>
+			</DialogContent>
+			<DialogActions>
+				<Button disabled={querying} onClick={props.close}>Cancel</Button>
+				<Button disabled={name === '' || querying} onClick={create}>Create</Button>
+			</DialogActions>
+		</Dialog>
+	)
+}
