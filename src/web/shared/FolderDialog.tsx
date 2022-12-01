fd9cd923426c851db3872fae6fa8054c542b1c95
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-12-01 19:31:30 +0900

    Folder is created upon pressing enter

--- src/web/shared/FolderDialog.tsx
+++ src/web/shared/FolderDialog.tsx
@@ -1,5 +1,5 @@
-import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button } from "@mui/material";
-import { useState } from "react";
+import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button, Box } from "@mui/material";
+import { FormEvent, useState } from "react";
 
 export function FolderDialog(props: {open: boolean, close: () => void, create: (name: string) => Promise<void>}){
 
@@ -14,7 +14,8 @@ export function FolderDialog(props: {open: boolean, close: () => void, create: (
 		}
 	}
 
-	const create = async () => {
+	const create = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 		try {
 			setQuerying(true);
 			await props.create(name);
@@ -28,15 +29,17 @@ export function FolderDialog(props: {open: boolean, close: () => void, create: (
 
 	return (
 		<Dialog open={props.open} onClose={onClose}>
-			<DialogTitle>Create new folder</DialogTitle>
-			<DialogContent>
-				<TextField
-					disabled={querying} required variant='standard' label='Name' type='name' value={name} error={error} helperText={error ? 'Cannot create folder.' : ' '} onChange={e => setName(e.currentTarget.value)}/>
-			</DialogContent>
-			<DialogActions>
-				<Button disabled={querying} onClick={props.close}>Cancel</Button>
-				<Button disabled={name === '' || querying} onClick={create}>Create</Button>
-			</DialogActions>
+			<Box component='form' onSubmit={create}>
+				<DialogTitle>Create new folder</DialogTitle>
+				<DialogContent>
+					<TextField
+						disabled={querying} required variant='standard' label='Name' type='name' value={name} error={error} helperText={error ? 'Cannot create folder. Perhaps you already have a folder with that name?' : ' '} onChange={e => setName(e.currentTarget.value)}/>
+				</DialogContent>
+				<DialogActions>
+					<Button disabled={querying} onClick={props.close}>Cancel</Button>
+					<Button type='submit' disabled={name === '' || querying}>Create</Button>
+				</DialogActions>
+			</Box>
 		</Dialog>
 	)
 }
