2da01b7efee0188a750444573bfb981a3f6e4f25
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-12-06 22:22:34 +0900

    More elaborate typing to track progress

--- src/web/sidebar/ItemDownload.tsx
+++ src/web/sidebar/ItemDownload.tsx
@@ -1,17 +1,41 @@
 import { Download } from "@mui/icons-material";
 import { ListItem, ListItemIcon, ListItemText } from "@mui/material";
 import { Item, EncryptedFile } from "cryptomator-ts";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { WebDAV } from "../../lib/cryptomator/WebDAV";
 
-export enum Progress{
+enum Step{
+	Starting,
 	Running,
+	Decrypting,
 	Done
 }
 
+type Progress = {
+	step: Step.Running | Step.Decrypting;
+	current: number;
+	total: number;
+} | {
+	step: Step.Done | Step.Starting;
+}
+
+/**
+ * Note:
+ * Add cleanup function that cancels download
+ */
+
 export function ItemDownload(props: {item: Item, client: WebDAV}){
 
-	const [prog, setProg] = useState<Progress>(Progress.Running);
+	const [prog, setProg] = useState<Progress>({
+		step: Step.Starting
+	});
+
+	const getMessage = () => {
+		if(prog.step === Step.Decrypting) return 'Decrypting...';
+		if(prog.step === Step.Done) return 'File downloaded.';
+		if(prog.step === Step.Running) return 'Downloading...';
+		if(prog.step === Step.Starting) return 'Fetching file details...';
+	}
 
 	return (
 		<ListItem>
@@ -24,11 +48,7 @@ export function ItemDownload(props: {item: Item, client: WebDAV}){
 					? props.item.decryptedName
 					: props.item.name
 				}
-				secondary={
-					prog === Progress.Running
-					? 'Downloading...'
-					: 'Download complete'
-				}
+				secondary={getMessage()}
 			/>
 		</ListItem>
 	)
