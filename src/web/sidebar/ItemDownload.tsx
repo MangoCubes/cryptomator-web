0acf970c2aef9197c369cd5e6b1812914498526c
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-12-06 22:28:27 +0900

    Added download on mount with callback

--- src/web/sidebar/ItemDownload.tsx
+++ src/web/sidebar/ItemDownload.tsx
@@ -1,6 +1,7 @@
 import { Download } from "@mui/icons-material";
 import { ListItem, ListItemIcon, ListItemText } from "@mui/material";
-import { Item, EncryptedFile } from "cryptomator-ts";
+import { Item, EncryptedFile, ProgressCallback } from "cryptomator-ts";
+import saveAs from "file-saver";
 import { useEffect, useState } from "react";
 import { WebDAV } from "../../lib/cryptomator/WebDAV";
 
@@ -29,6 +30,36 @@ export function ItemDownload(props: {item: Item, client: WebDAV}){
 	const [prog, setProg] = useState<Progress>({
 		step: Step.Starting
 	});
+
+	const downloadCallback: ProgressCallback = (c, t) => setProg({
+		step: Step.Running,
+		current: c,
+		total: t
+	});
+
+	const decryptionCallback: ProgressCallback = (c, t) => setProg({
+		step: Step.Decrypting,
+		current: c,
+		total: t
+	});
+
+	const startLoad = async () => {
+		if (props.item instanceof EncryptedFile) {
+			const decrypted = await props.item.decrypt({
+				download: downloadCallback,
+				decrypt: decryptionCallback
+			});
+			saveAs(new Blob([decrypted.content]), decrypted.title);
+		}
+		else {
+			const data = await props.client.readFile(props.item.fullName, downloadCallback);
+			saveAs(new Blob([data]), props.item.name);
+		}
+	}
+
+	useEffect(() => {
+		startLoad();
+	}, [])
 
 	const getMessage = () => {
 		if(prog.step === Step.Decrypting) return 'Decrypting...';
