ef1c54ea37684e46fcbc1da245db47f0a9e21164
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-12-11 22:17:55 +0900

    Link does not go multiple lines if the file name is too long

--- src/web/shared/DirBreadcrumbs.tsx
+++ src/web/shared/DirBreadcrumbs.tsx
@@ -1,19 +1,27 @@
-import { Typography, Link, Box, Breadcrumbs } from "@mui/material";
+import { Link, Box, Breadcrumbs, styled } from "@mui/material";
+import { SingleLine } from "./SingleLine";
+
+const SingleLineLink = styled(Link)({
+	textOverflow: 'ellipsis',
+	whiteSpace: 'nowrap',
+	overflow: 'hidden',
+	maxWidth: '20vw'
+});
 
 export function DirBreadcrumbs(props: {dir: string[], cd: (index: number) => void}) {
 	const prev = [
 		props.dir.length === 0
-		? <Typography color='text.primary' key='Root'>Root</Typography>
-		: <Link underline='hover' key='root' href='#' onClick={() => props.cd(0)}>Root</Link>,
+		? <SingleLine color='text.primary' key='Root' maxWidth='20vw'>Root</SingleLine>
+		: <SingleLineLink underline='hover' key='root' href='#' onClick={() => props.cd(0)}>Root</SingleLineLink>,
 		...props.dir.slice(0, -1).map((d, i) => (
-			<Link underline='hover' key={i} href='#' onClick={() => props.cd(i + 1)}>{d}</Link>
-		))			
+			<SingleLineLink underline='hover' key={i} href='#' onClick={() => props.cd(i + 1)}>{d}</SingleLineLink>
+		))
 	];
 	return (
 		<Box mt={1} ml={1}>
-			<Breadcrumbs>
+			<Breadcrumbs maxItems={4}>
 				{prev}
-				<Typography color='text.primary'>{props.dir[props.dir.length - 1]}</Typography>
+				<SingleLine color='text.primary' maxWidth='20vw'>{props.dir[props.dir.length - 1]}</SingleLine>
 			</Breadcrumbs>
 		</Box>
 	);
